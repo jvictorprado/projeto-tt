bf6bdd3e4a10125af2aad209e50e0104ab82bc42
Victor Prado <victorpradoestudo@gmail.com>
2021-10-23 18:25:25 -0300

    user

---
+++ db/migrate/20211023205630_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :nome
+      t.string :senha
+      t.string :email
+      t.string :cpf
+
+      t.timestamps
+    end
+  end
+end
