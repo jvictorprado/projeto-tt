a6e4cf61046c44865d0a806a1906e2c84493f5e3
Victor Prado <victorpradoestudo@gmail.com>
2021-10-28 22:42:01 -0300

    modif-db

--- db/migrate/20211023205630_create_users.rb
+++ db/migrate/20211023205630_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :nome
-      t.string :senha
+      t.string :password_digest
       t.string :email
       t.string :cpf
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_23_205630) do
+ActiveRecord::Schema.define(version: 2021_10_26_214218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "nome"
-    t.string "senha"
+    t.string "password_digest"
     t.string "email"
     t.string "cpf"
     t.datetime "created_at", precision: 6, null: false
